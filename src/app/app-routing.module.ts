9acbfc3444d76d4b56cfedb78ac181b2f64dc807
nikito96 <nikinikolitsov9655@gmail.com>
2018-05-29 00:44:55 +0300

    about component added to the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'home', component: AppComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
+  exports: [ RouterModule ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
   declarations: []
 })

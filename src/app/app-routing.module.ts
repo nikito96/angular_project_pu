83abae95208fa6674be6103a524ef7b0080c4555
nikito96 <nikinikolitsov9655@gmail.com>
2018-05-29 01:29:45 +0300

    building site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
-  { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
